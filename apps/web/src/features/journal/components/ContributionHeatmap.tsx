ea470f256a6444e53d21d5b56f2d6f0988725093
ajmal faris <ajmalfaris.me@gmail.com>
2026-05-25 19:52:18 +0530

    chore: clean up unused code in ContributionHeatmap.tsx

--- apps/web/src/features/journal/components/ContributionHeatmap.tsx
+++ apps/web/src/features/journal/components/ContributionHeatmap.tsx
@@ -1,5 +1,6 @@
 import { motion } from "framer-motion";
 import React, { useEffect, useRef } from "react";
+
 import { LEVEL_COLORS } from "../data/constants";
 import { HeatmapDay } from "../types";
 
